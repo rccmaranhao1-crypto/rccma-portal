1b0e23cf6c55d9e7064c499deec8ebd439df0474
rccmaranhao1-crypto <rccmaranhao1@gmail.com>
2026-02-24 21:19:50 -0300

    Refactor enableShutdownHooks method syntax

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -7,9 +7,8 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
     await this.$connect();
   }
 
-  async enableShutdownHooks(app: INestApplication) {
-    this.$on("beforeExit", async () => {
-      await app.close();
-    });
-  }
+ async enableShutdownHooks(app: INestApplication) {
+  this.$on('beforeExit' as any, async () => {
+    await app.close();
+  });
 }
