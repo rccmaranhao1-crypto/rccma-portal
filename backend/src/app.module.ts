27b34a2745a3f4ac832ff97517c4dbc3f46f68e6
rccmaranhao1-crypto <rccmaranhao1@gmail.com>
2026-02-24 21:25:52 -0300

    Refactor AppModule to include ServeStaticModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,42 +1,35 @@
 import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
+
 import { PrismaModule } from "./prisma/prisma.module";
 import { AuthModule } from "./modules/auth/auth.module";
 import { UsersModule } from "./modules/users/users.module";
 import { DioceseModule } from "./modules/diocese/diocese.module";
 import { DonationsModule } from "./modules/donations/donations.module";
+import { CampaignsModule } from "./modules/campaigns/campaigns.module";
 import { ProductsModule } from "./modules/products/products.module";
 import { OrdersModule } from "./modules/orders/orders.module";
-import { CampaignsModule } from "./modules/campaigns/campaigns.module";
 import { PaymentsModule } from "./modules/payments/payments.module";
 import { UploadsModule } from "./modules/uploads/uploads.module";
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), "uploads"),
+      serveRoot: "/uploads",
+    }),
+
     PrismaModule,
     AuthModule,
     UsersModule,
     DioceseModule,
-    PaymentsModule,
-    UploadsModule,
     DonationsModule,
+    CampaignsModule,
     ProductsModule,
     OrdersModule,
-    CampaignsModule,
-  ],
-})
-export class AppModule {}
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
-
-@Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(process.cwd(), 'uploads'),
-      serveRoot: '/uploads',
-    }),
-    // ...outros imports
+    PaymentsModule,
+    UploadsModule,
   ],
 })
 export class AppModule {}
