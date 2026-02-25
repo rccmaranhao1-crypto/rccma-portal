3d18ed365a1f276eca9999602eb25825f9f72be2
rccmaranhao1-crypto <rccmaranhao1@gmail.com>
2026-02-24 22:29:19 -0300

    Update prisma.service.ts

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,16 +1,13 @@
-import { INestApplication, Injectable, OnModuleInit } from "@nestjs/common";
+import { Injectable, OnModuleInit, OnModuleDestroy } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   async onModuleInit() {
     await this.$connect();
   }
 
-  async enableShutdownHooks(app: INestApplication) {
-    // Prisma v5 em alguns ambientes tipa `beforeExit` de forma restritiva
-    this.$on("beforeExit" as any, async () => {
-      await app.close();
-    });
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
