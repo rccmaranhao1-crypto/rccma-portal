cc5100fb547cbb8f053ba9a631b7a65cc87ed88d
rccmaranhao1-crypto <rccmaranhao1@gmail.com>
2026-02-28 10:39:59 -0300

    Fix import statement for PaymentsModule

--- backend/src/modules/payments/payments.module.ts
+++ backend/src/modules/payments/payments.module.ts
@@ -1,6 +1,6 @@
 import { Module } from "@nestjs/common";
 import { PaymentsService } from "./payments.service";
-import { PaymentsController } from "./payments.controller";
+import { PaymentsModule } from '../payments/payments.module';
 
 @Module({
   providers: [PaymentsService],
