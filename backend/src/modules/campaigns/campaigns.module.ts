12a1adb5f92fb808b373caad5246c90ab6f7f90d
rccmaranhao1-crypto <rccmaranhao1@gmail.com>
2026-02-28 10:19:56 -0300

    Add PaymentsModule import to CampaignsModule

--- backend/src/modules/campaigns/campaigns.module.ts
+++ backend/src/modules/campaigns/campaigns.module.ts
@@ -1,8 +1,10 @@
-import { Module } from "@nestjs/common";
-import { CampaignsController } from "./campaigns.controller";
-import { CampaignsService } from "./campaigns.service";
+import { Module } from '@nestjs/common';
+import { CampaignsService } from './campaigns.service';
+import { CampaignsController } from './campaigns.controller';
+import { PaymentsModule } from '../payments/payments.module';
 
 @Module({
+  imports: [PaymentsModule],
   controllers: [CampaignsController],
   providers: [CampaignsService],
 })
