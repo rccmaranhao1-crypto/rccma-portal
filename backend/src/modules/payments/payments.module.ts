18deb94707d35991b6467d9098dc1d97ed6be7a2
rccmaranhao1-crypto <rccmaranhao1@gmail.com>
2026-02-28 11:00:18 -0300

    Remove forwardRef from OrdersModule import

--- backend/src/modules/payments/payments.module.ts
+++ backend/src/modules/payments/payments.module.ts
@@ -1,12 +1,10 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
-import { OrdersModule } from '../orders/orders.module';
 
 @Module({
-  imports: [forwardRef(() => OrdersModule)],
   providers: [PaymentsService],
   controllers: [PaymentsController],
-  exports: [PaymentsService],
+  exports: [PaymentsService], // <-- obrigatório
 })
 export class PaymentsModule {}
