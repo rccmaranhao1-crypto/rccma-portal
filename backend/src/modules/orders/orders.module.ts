c5590005e1c221157a8478119b09bcf98c8d19a6
rccmaranhao1-crypto <rccmaranhao1@gmail.com>
2026-02-28 10:33:42 -0300

    Add PaymentsModule to OrdersModule imports

--- backend/src/modules/orders/orders.module.ts
+++ backend/src/modules/orders/orders.module.ts
@@ -1,8 +1,10 @@
-import { Module } from "@nestjs/common";
-import { OrdersController } from "./orders.controller";
-import { OrdersService } from "./orders.service";
+import { Module } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { OrdersController } from './orders.controller';
+import { PaymentsModule } from '../payments/payments.module';
 
 @Module({
+  imports: [PaymentsModule],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
