d0268469ae4cfde4d72271224dee42657090d15e
rccmaranhao1-crypto <rccmaranhao1@gmail.com>
2026-02-28 11:23:05 -0300

    Update campaigns.module.ts

--- backend/src/modules/campaigns/campaigns.module.ts
+++ backend/src/modules/campaigns/campaigns.module.ts
@@ -1,6 +1,3 @@
-import { Module } from '@nestjs/common';
-import { CampaignsService } from './campaigns.service';
-import { CampaignsController } from './campaigns.controller';
 import { PaymentsModule } from '../payments/payments.module';
 
 @Module({
