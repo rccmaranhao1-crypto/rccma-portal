89956de3d9db4efee3407c541e8279e5fd30ad09
rccmaranhao1-crypto <rccmaranhao1@gmail.com>
2026-02-28 10:55:24 -0300

    Update payments.module.ts

--- backend/src/modules/payments/payments.module.ts
+++ backend/src/modules/payments/payments.module.ts
@@ -1,8 +1,10 @@
-import { Module } from "@nestjs/common";
-import { PaymentsService } from "./payments.service";
-import { PaymentsModule } from '../payments/payments.module';
+import { Module, forwardRef } from '@nestjs/common';
+import { PaymentsService } from './payments.service';
+import { PaymentsController } from './payments.controller';
+import { OrdersModule } from '../orders/orders.module';
 
 @Module({
+  imports: [forwardRef(() => OrdersModule)],
   providers: [PaymentsService],
   controllers: [PaymentsController],
   exports: [PaymentsService],
